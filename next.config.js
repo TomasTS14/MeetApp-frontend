9a8062afae58354b0d0abf502121c7f20dd4a699
Tomas Casa WIn <210099@fppiramide.com>
2023-06-09 19:53:16 +0200

    new redirect with port and http

--- next.config.js
+++ next.config.js
@@ -13,11 +13,11 @@ const nextConfig = {
     return [
       {
         source: "/api/events/near_me",
-        destination: "meetapp-backend-production.up.railway.app/events/near_me"
+        destination: "http://meetapp-backend-production.up.railway.app:8080/events/near_me"
       },
       {
         source: "/api/events/liked/:id",
-        destination: "meetapp-backend-production.up.railway.app/events/liked/:id"
+        destination: "http://meetapp-backend-production.up.railway.app:8080/events/liked/:id"
       },
       {
         source: "/api/events/interaction/:eventId*",
@@ -28,43 +28,43 @@ const nextConfig = {
             value: 'true' || 'false'
           }
         ],
-        destination: "meetapp-backend-production.up.railway.app/events/interaction/:eventId*"
+        destination: "http://meetapp-backend-production.up.railway.app:8080/events/interaction/:eventId*"
       },
       {
         source: "/api/events/my_events",
-        destination: "meetapp-backend-production.up.railway.app/events/my_events"
+        destination: "http://meetapp-backend-production.up.railway.app:8080/events/my_events"
       },
       {
         source: "/api/events/save",
-        destination: "meetapp-backend-production.up.railway.app/events/save"
+        destination: "http://meetapp-backend-production.up.railway.app:8080/events/save"
       },
       {
         source: "/api/events/delete/:eventId",
-        destination: "meetapp-backend-production.up.railway.app/events/delete/:eventId"
+        destination: "http://meetapp-backend-production.up.railway.app:8080/events/delete/:eventId"
       },
       {
         source: "/api/events/interaction/:eventId",
-        destination: "meetapp-backend-production.up.railway.app/events/interaction/:eventId"
+        destination: "http://meetapp-backend-production.up.railway.app:8080/events/interaction/:eventId"
       },
       {
         source: "/api/events/:id",
-        destination: "meetapp-backend-production.up.railway.app/events/:id"
+        destination: "http://meetapp-backend-production.up.railway.app:8080/events/:id"
       },
       {
         source: "/api/users/signup",
-        destination: "meetapp-backend-production.up.railway.app/users/signup"
+        destination: "http://meetapp-backend-production.up.railway.app:8080/users/signup"
       },
       {
         source: "/api/users/login",
-        destination: "meetapp-backend-production.up.railway.app/users/login",
+        destination: "http://meetapp-backend-production.up.railway.app:8080/users/login",
       },
       {
         source: "/api/users/:slug",
-        destination: "meetapp-backend-production.up.railway.app/users/:slug",
+        destination: "http://meetapp-backend-production.up.railway.app:8080/users/:slug",
       },
       {
         source: "/api/users/check/params",
-        destination: "meetapp-backend-production.up.railway.app/users/check/params"
+        destination: "http://meetapp-backend-production.up.railway.app:8080/users/check/params"
       }
     ];
   },
